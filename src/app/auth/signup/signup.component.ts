a1fb1a4ba3edc7fadf2743ee95e6db2cf3161d89
rutv1k46 <rutviksolanki01@gmail.com>
2024-08-04 16:52:10 -0400

    reactive form - nested form groups

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import {
     AbstractControl,
+    FormArray,
     FormControl,
     FormGroup,
     ReactiveFormsModule,
@@ -37,25 +38,36 @@ export class SignupComponent {
             validators: [Validators.email, Validators.required],
             asyncValidators: [emailIsUnique],
         }),
-        password: new FormControl("", {
-            validators: [
-                Validators.required,
-                Validators.minLength(6),
-                // mustContainQuestionMark,
-            ],
-        }),
-        confirmPassword: new FormControl("", {
-            validators: [Validators.required, Validators.minLength(6)],
+        passwords: new FormGroup({
+            password: new FormControl("", {
+                validators: [
+                    Validators.required,
+                    Validators.minLength(6),
+                    // mustContainQuestionMark,
+                ],
+            }),
+            confirmPassword: new FormControl("", {
+                validators: [Validators.required, Validators.minLength(6)],
+            }),
         }),
         firstName: new FormControl("", { validators: [Validators.required] }),
         lastName: new FormControl("", { validators: [Validators.required] }),
-        street: new FormControl("", { validators: [Validators.required] }),
-        number: new FormControl("", { validators: [Validators.required] }),
-        postalCode: new FormControl("", { validators: [Validators.required] }),
-        city: new FormControl("", { validators: [Validators.required] }),
+        address: new FormGroup({
+            street: new FormControl("", { validators: [Validators.required] }),
+            number: new FormControl("", { validators: [Validators.required] }),
+            postalCode: new FormControl("", {
+                validators: [Validators.required],
+            }),
+            city: new FormControl("", { validators: [Validators.required] }),
+        }),
         role: new FormControl<
             "student" | "teacher" | "employee" | "founder" | "other"
         >("student", { validators: [Validators.required] }),
+        source: new FormArray([
+            new FormControl(false),
+            new FormControl(false),
+            new FormControl(false),
+        ]),
         agree: new FormControl(false, { validators: [Validators.required] }),
     });
 
