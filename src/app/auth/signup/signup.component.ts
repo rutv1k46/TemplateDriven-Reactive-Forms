c8d28c1dcc7bbf2af4f31072d39c674bb8101271
rutv1k46 <rutviksolanki01@gmail.com>
2024-08-04 16:20:00 -0400

    reactive form - sign-up form

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,56 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+    AbstractControl,
+    FormControl,
+    FormGroup,
+    ReactiveFormsModule,
+    Validators,
+} from "@angular/forms";
+import { of } from "rxjs";
+
+function mustContainQuestionMark(control: AbstractControl) {
+    if (control.value.includes("?")) {
+        return null;
+    }
+
+    return { doesNotContainQuestionMark: true };
+}
+
+function emailIsUnique(control: AbstractControl) {
+    if (control.value !== "rutv1k46@gwu.edu") {
+        return of(null);
+    }
+
+    return of({ notUnique: true });
+}
 
 @Component({
-  selector: 'app-signup',
-  standalone: true,
-  templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css',
+    selector: "app-signup",
+    standalone: true,
+    imports: [ReactiveFormsModule],
+    templateUrl: "./signup.component.html",
+    styleUrl: "./signup.component.css",
 })
-export class SignupComponent {}
+export class SignupComponent {
+    form = new FormGroup({
+        email: new FormControl("", {
+            validators: [Validators.email, Validators.required],
+            asyncValidators: [emailIsUnique],
+        }),
+        password: new FormControl("", {
+            validators: [
+                Validators.required,
+                Validators.minLength(6),
+                // mustContainQuestionMark,
+            ],
+        }),
+    });
+
+    onSubmit() {
+        console.log(this.form);
+    }
+
+    onReset(){
+      this.form.reset()
+    }
+}
